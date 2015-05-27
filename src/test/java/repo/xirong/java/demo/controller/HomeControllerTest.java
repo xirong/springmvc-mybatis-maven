2f1805fd330f2936163d82ce93e948ef012586e1
xirong <jackieliuxirong@gmail.com>
2015-05-27 23:29:13 +0800

    add test dir

---
+++ src/test/java/repo/xirong/java/demo/controller/HomeControllerTest.java
@@ -0,0 +1,15 @@
+package repo.xirong.java.demo.controller;
+
+import junit.framework.TestCase;
+
+public class HomeControllerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+}
